c0d91f61643450e9ebc02201c643d60732748a7f
muz721 <1248562370@qq.com>
2019-05-08 19:28:50 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-module.exports = {
-    devServer: {
-        proxy: {
-            '/apis': {
-                target: 'http://www.wanandroid.com/', // target host
-                ws: true, // proxy websockets 
-                changeOrigin: true, // needed for virtual hosted sites
-                pathRewrite: {
-                    '^/apis': '' // rewrite path
-                }
-            },
-        }
-    }
-};
 init();
 function init() {
     var prime_search =document.getElementById("prime_searsh");
